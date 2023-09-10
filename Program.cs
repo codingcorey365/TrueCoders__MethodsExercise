89ceeae7c7e027f3eb809f5b4b51d2cfd14d58bc
Corey Schroeder <codingcorey365@gmail.com>
2023-09-10 17:13:42 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MethodsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------------------Exercise 1--------------------------------------------------------------------
+            //Name: Michael
+            //Favorite Color: Blue
+            //Favorite Animal: Walrus
+            //Favorite Band: The Beatles
+
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hi, What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {userName}, what is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now, what is your favorite band?");
+            var band = Console.ReadLine(); Console.WriteLine("Hello, World!");
+        }
+    }
+}
